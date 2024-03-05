604b2f25a38f62d4cb94518a9aeb32b17e4325c0
3174 <3174@LAB1-PROF>
2024-03-05 19:10:36 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/CalculaIMC.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01
+{
+    public class CalculaIMC
+    {
+        public string Calcular(int peso, float altura)
+        {
+            float imc = peso / (altura * altura);
+            // Console.WriteLine(imc);
+            string resultado = "";
+            if (imc <= 18.5)
+            {
+                resultado = "Magreza";
+            }
+            else if (imc <= 24.9)
+            {
+                resultado = "peso normal";
+            }
+            else if (imc <= 29.9)
+            {
+                resultado = "sobrepeso";
+            }
+            else if (imc <= 34.9)
+            {
+                resultado = "obesidade grau 1";
+            }
+            else if (imc <= 49.9)
+            {
+                resultado = "obesidade grau 2";
+            }
+            else
+            {
+                resultado = "obesidade grau 3";
+            }
+            string retorno = $"O IMC calculado para o peso {peso} e a altura {altura} foi {imc:f1} classificado como {resultado}";
+            return retorno;
+        }
+    }
+}
---
+++ Aula01/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//altere o programa para calcular o imc de uma pessoa
+// imc = peso / altura * altura
+using System.Globalization;
+
+bool entrada = true;
+do
+{
+    try
+    {
+        Console.WriteLine("Informe o peso atual: ");
+        int peso = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Informe a altura atual: ");
+        float altura = float.Parse(Console.ReadLine());
+        entrada = false;
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine("O peso deve ser um número inteiro e a altura em numero decimal");
+    }
+} while (entrada);
+
+
+
